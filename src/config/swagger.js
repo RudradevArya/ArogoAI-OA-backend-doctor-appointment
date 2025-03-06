d535b0ee375553e994a2047bcde15068f2596284
Rudradev-Arya <rudradev93@gmail.com>
2025-03-07 04:15:17 +0530

    fix: AWS beanstalk deployment conflicts depricated commands

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000',
+        url: 'https://arogoai-oa-backend-doctor-appointment.onrender.com' || 'http://localhost:5000',
         description: 'Development server',
       },
     ],
